13e8a48c44183c5f483ebc6adbbe377f5de1cae5
guptvarun619 <guptavarun619@gmail.com>
2020-04-30 17:20:34 +0530

    Binary search on 2D sorted array

--- Array/SortedMatrixSearch.cpp
+++ Array/SortedMatrixSearch.cpp
@@ -7,7 +7,7 @@ int main()
         freopen("input.txt", "r", stdin);
     #endif
     int a[100][100], key, row, col;
-    cin >> row >> col;
+    cin >> row >> col >> key;
     // NOTE THE ARRAY SHOULD BE SORTED ROW AND COL WISE
     for (int i = 0; i < row; i++)
     {
@@ -17,7 +17,22 @@ int main()
         }        
     }
 
-    
+    int i = 0, j = col - 1;
+    while(i < row  && col > 0)
+    {
+        if(a[i][j] < key)
+            i++;
+        else if(a[i][j] > key)
+            j--;
+        if(a[i][j] == key)
+        {
+            cout << "Element found at " << i << "," << j << endl;
+            break;
+        }
+    }
+
+    if(a[i][j] != key)
+        cout << "Element not found!" << endl;
     
     return 0;
 }
