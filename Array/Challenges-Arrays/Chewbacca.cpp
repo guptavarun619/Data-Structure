bacb7dd3f7a9c63e26bb79791bb6d7c53875d88d
guptvarun619 <guptavarun619@gmail.com>
2020-05-05 23:50:51 +0530

    Chewbacca number problem

--- Array/Challenges-Arrays/Chewbacca.cpp
+++ Array/Challenges-Arrays/Chewbacca.cpp
@@ -7,67 +7,26 @@ int main()
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
     #endif
-    int n;
-    bool positive = true;
+    string n;
     cin >> n;
-    positive = n < 0 ? false : true;
-    vector<int> num;
-    int tmp;
-    if(positive)
+
+    int i = 0;
+    if(n[i] != '9' && n[i] >= '5') 
     {
-        while (n > 0){
-            tmp = n % 10;
-            num.push_back(tmp);
-            n /= 10;
-        }
-    }
-    else
+        n[i] = (char) ('9' - n[i]) + 48 ;
+    }    
+    cout << n[i];  
+
+    for (i = 1; i < n.size(); i++)
     {
-        while (n < 0) {
-            tmp = (n % 10) * -1;
-            num.push_back(tmp);
-            n /= 10;
-        }
+        if(n[i] >= '5') 
+        {
+            n[i] = (char) ('9' - n[i]) + 48 ;
+        }    
+        cout << n[i];
     }
     
-    int number = 0, p = 1;
-    int i =0;
-    if(positive)
-    {
-        for (; i < num.size()-1; i++)
-        {        
-            if(num[i] < 5)
-                number += num[i] * p;
-            else 
-                number += (9 - num[i]) * p;
-            p *= 10;
-        }
-        if(num[i] > 4 || num[i] == 9)
-            number += (9 - num[i]) * p;
-        else 
-            number += num[i] * p;
-    }
-    else
-    {
-        for (; i < num.size()-1; i++)
-        {        
-            if(num[i] < 5)
-                number += (9 - num[i]) * p;
-            else 
-                number += num[i] * p;
-            p *= 10;
-        }
-        if(num[i] > 4 || num[i] == 9)
-            number += num[i] * p;
-        else 
-            number += (9 - num[i]) * p;
-    }
     
-
-
-    if(!positive)
-        cout << "-";
-    cout << number;
 
     return 0;
 }
