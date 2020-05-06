5f9150e84ff46c22856d5bce2934fb316d509e39
guptvarun619 <guptavarun619@gmail.com>
2020-05-06 20:52:21 +0530

    Sum of two numbers using array problem completed

--- Array/Challenges-Arrays/ArraySumTwoArray.cpp
+++ Array/Challenges-Arrays/ArraySumTwoArray.cpp
@@ -12,43 +12,55 @@ int main()
     for(int i = n - 1;i>=0;i--)
     {
         cin >> a[i];
-        cout << a[i] << " ";
     }
+    
     cout << endl;
+    
     cin >> m;
     int b[m];
+
     for(int i = m - 1;i>=0;i--)
     {
         cin >> b[i];
-        cout << b[i] << " ";
     }   
     cout <<endl;
-    int length = n > m ? n+1 : m+1;
+    int length = max(m, n) + 1;
     int sum[length] = {0};
     bool carry = false;
+
     for(int i=0; i<length; i++)
     {
         if(carry)
+        {
             sum[i] += 1;
+            carry = false;
+        }
             
-        if(length - 1 >=n)
+        if(i < n)
         {
             if(sum[i] + a[i] > 9)
                 carry = true;
             sum[i] = (sum[i] + a[i])%10;
+            // cout << a[i] << " ";
         }
-        if(length - 1 >=m)
+
+        if(i < m)
         {
             if(sum[i] + b[i] > 9)
                 carry = true;
             sum[i] = (sum[i] + b[i])%10;
+            // cout << b[i] << endl;
         }    
         
     }
-    for(int i = length; i>=0; i--)
+    int i = length - 1;
+    if(sum[i])
+        cout << sum[i] << ", ";
+    for (i = length - 2; i >= 0; i--)
     {
         cout << sum[i] << ", ";
-    }   
+    }
+    
     cout << "END";
     
     return 0;
