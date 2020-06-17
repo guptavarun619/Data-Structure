ab42c9ddf419806144867c0932aae3789a4a361e
guptvarun619 <guptavarun619@gmail.com>
2020-06-17 23:06:30 +0530

    Winning CB scholarship problem solved

---
+++ DivideNConquer/WinningCB.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    #endif
+
+	int n, m, x, y;
+	cin >> n >> m >> x >> y;
+	int s = 0;
+	int e = n;
+	int ans = -1;
+	while(s<=e)
+	{
+		int mid = s + ((e-s)/2);
+		if (mid*x <= m + ((n - mid)*y))
+		{
+			ans = mid;
+			s = mid + 1;
+		}
+		else 
+			e = mid - 1;
+	}
+    cout << ans;
+	return 0;
+}
