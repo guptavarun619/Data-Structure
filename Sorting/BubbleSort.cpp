81daa022acd74150d3b7b7ec6afbe14f6904c95f
guptvarun619 <guptavarun619@gmail.com>
2020-07-17 12:29:19 +0530

    Bubble sort implemented

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void cpSetup() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    #endif
+}
+
+int32_t main() {
+    cpSetup();
+
+    int n;
+    cin >> n;
+    int *a;
+    a = new int[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    // Bubble Sort Algorithm
+    
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            if(a[j] > a[j+1])
+                swap(a[j], a[j+1]);
+        }
+        cout << i+1 << " pass : ";
+        for(int i = 0; i < n; i++)
+            cout << a[i] << " ";
+        cout << endl;
+    }
+
+    cout << "Sorted array is : ";
+    for(int i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
