7b8c6677bd26319d021942314d217a7671e06193
guptvarun619 <guptavarun619@gmail.com>
2020-07-17 14:34:01 +0530

    Minimum Passes required to sort the array

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -24,11 +24,17 @@ int32_t main() {
     
     for(int i = 0; i < n-1; i++)
     {
+        int flag = 0;
         for(int j = 0; j < n-i-1; j++)
         {
             if(a[j] > a[j+1])
+            {
                 swap(a[j], a[j+1]);
+                flag = 1;
+            }
         }
+        if(flag == 0)
+            break;
         cout << i+1 << " pass : ";
         for(int i = 0; i < n; i++)
             cout << a[i] << " ";
